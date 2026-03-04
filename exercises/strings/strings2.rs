701c6636756e85b8b3918d11dd4c10dc8af8c8f5
Raczyx <1596700738@qq.com>
2026-03-04 22:15:00 +0800

    update

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -16,6 +16,6 @@ fn main() {
     }
 }
 
-fn is_a_color_word(attempt: &str) -> bool {
-    attempt == "green" || attempt == "blue" || attempt == "red"
+fn is_a_color_word(attempt: String) -> bool {
+    attempt.eq("green") || attempt.eq("blue") || attempt.eq("red")
 }
